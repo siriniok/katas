50e95c0c371610a18f4da5baa5f0252df9139de7
Vladyslav Siriniok <siriniok@gmail.com>
2018-05-21 06:24:07 +0300

    Solve the English numbers kata

--- eng-nums/eng_nums.rb
+++ eng-nums/eng_nums.rb
@@ -1,3 +1,22 @@
-class EngNums
-  
+SIMPLE_NUMBERS = %w(
+  zero one two three four
+  five six seven eight nine
+
+  ten eleven twelve thirteen fourteen
+  fifteen sixteen seventeen eighteen nineteen
+)
+
+TENS = %w(
+  \'' \'' twenty thirty fourty fifty sixty seventy eighty ninety
+)
+
+def to_english_number(number)
+  if number >= 20
+    first_part  = TENS[number / 10 ]
+    second_part = SIMPLE_NUMBERS[number % 10]
+
+    return first_part + (second_part == 'zero' ? '' : "-#{second_part}")
+  end
+
+  SIMPLE_NUMBERS[number]
 end
--- eng-nums/eng_nums_spec.rb
+++ eng-nums/eng_nums_spec.rb
@@ -3,5 +3,48 @@ $LOAD_PATH.unshift File.expand_path('../.', __FILE__)
 require 'byebug'
 require 'eng_nums'
 
-describe EngNums do
+SIMPLE_TEST_NUMBERS = %w(
+  zero one two three four
+  five six seven eight nine
+
+  ten eleven twelve thirteen fourteen
+  fifteen sixteen seventeen eighteen nineteen
+)
+
+describe '#to_english_number' do
+  (0..19).each do |number|
+    expected_english_number = SIMPLE_TEST_NUMBERS[number]
+
+    it "converts #{number} to #{expected_english_number}" do
+      expect(to_english_number(number)).to eq(expected_english_number )
+    end
+  end
+
+  it 'converts 20 to twenty' do
+    expect(to_english_number(20)).to eq('twenty')
+  end
+
+  it 'converts 21 to twenty-one' do
+    expect(to_english_number(21)).to eq('twenty-one')
+  end
+
+  it 'converts 25 to twenty-five' do
+    expect(to_english_number(25)).to eq('twenty-five')
+  end
+
+  it 'converts 30 to thirty' do
+    expect(to_english_number(30)).to eq('thirty')
+  end
+
+  it 'converts 42 to fourty-two' do
+    expect(to_english_number(42)).to eq('fourty-two')
+  end
+
+  it 'converts other 2-digit numbers' do
+    expect(to_english_number(33)).to eq('thirty-three')
+    expect(to_english_number(56)).to eq('fifty-six')
+    expect(to_english_number(57)).to eq('fifty-seven')
+    expect(to_english_number(72)).to eq('seventy-two')
+    expect(to_english_number(99)).to eq('ninety-nine')
+  end
 end
