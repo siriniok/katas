6f8ad87f29a1a88985eb0f30f66752b7305e3ff5
Vladyslav Siriniok <siriniok@gmail.com>
2018-06-15 15:07:00 +0300

    Implement bowling kata

--- bowling_kata/lib/game.rb
+++ bowling_kata/lib/game.rb
@@ -1,1 +1,56 @@
-# Start writing your code here
+class Game
+  MAX_PINS  = 10
+  FRAMES    = 1..10
+
+  def initialize
+    @rolls = []
+  end
+
+  def roll(pins)
+    @rolls << pins
+  end
+
+  def score
+    strikes = 0
+
+    FRAMES.reduce(0) do |score, frame_index|
+      r1, r2, r3  = three_next_rolls(frame_index - 1, strikes)
+      bonus_score = 0
+
+      if strike?(r1)
+        frame = [r1]
+        frame_sum = sum_frame(frame)
+        bonus_score = r2 + r3
+        strikes += 1
+      else
+        frame      = [r1, r2]
+        frame_sum = sum_frame(frame)
+
+        bonus_score = r3 if spare?(frame, frame_sum)
+      end
+
+      score + frame_sum + bonus_score
+    end
+  end
+
+  private
+  ############################################################################
+
+  def three_next_rolls(frame_index, strikes)
+    first = frame_index * 2 - strikes
+    last  = frame_index * 2 + 2
+    @rolls[first..last]
+  end
+
+  def sum_frame(frame)
+    frame.reduce(:+)
+  end
+
+  def spare?(frame, frame_sum)
+    frame.first != MAX_PINS && frame_sum == MAX_PINS
+  end
+
+  def strike?(pins)
+    pins == MAX_PINS
+  end
+end
--- bowling_kata/spec/game_spec.rb
+++ bowling_kata/spec/game_spec.rb
@@ -1,8 +1,75 @@
 require 'spec_helper'
 require 'game'
 
-describe 'FizzBuzz' do
-  it 'is a Fizz and Buzz' do
-    expect(false).to be_true
+describe Game do
+  MAX_ROLLS = 20
+
+  let(:game) { subject }
+
+  def roll_many(n, pins)
+    n.times do
+      game.roll(pins)
+    end
+  end
+
+  def roll_frame(first_roll, second_roll)
+    game.roll(first_roll)
+    game.roll(second_roll)
+  end
+
+  def roll_spare(first)
+    roll_frame(first, described_class::MAX_PINS - first)
+  end
+
+  def roll_strike
+    game.roll(described_class::MAX_PINS)
+  end
+
+  context 'when a player plays all rolls in a gutter' do
+    it 'scores 0' do
+      pins = 0
+
+      roll_many(MAX_ROLLS, pins)
+
+      expect(game.score).to eq(0)
+    end
+  end
+
+  context 'when a player knocks down only one pin in all rolls' do
+    it 'scores 20' do
+      pins = 1
+
+      roll_many(MAX_ROLLS, pins)
+
+      expect(game.score).to eq(20)
+    end
+  end
+
+  context 'when a player makes a single spare' do
+    it 'scores max pins plus the next roll twice' do
+      roll_spare(6)
+      roll_frame(3, 1)
+      roll_many(16, 0)
+
+      expect(game.score).to eq(17)
+    end
+  end
+
+  context 'when a player makes a single strike' do
+    it 'scores max pins plus the next two rolls twice' do
+      roll_strike
+      roll_frame(3, 1)
+      roll_many(16, 0)
+
+      expect(game.score).to eq(18)
+    end
+  end
+
+  context 'when a player has a perfect game' do
+    it 'scores 300' do
+      roll_many(12, 10)
+
+      expect(game.score).to eq(300)
+    end
   end
 end
